e4c8493335930acc014c356d66da68b15f52a05d
kellymokeefe <128492828+kellymokeefe@users.noreply.github.com>
2024-02-09 19:09:58 +0100

    Added Dictionary component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 
+import Dictionary from './Dictionary';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className='container'>
       <header className="App-header">
-      <h1>Hello world</h1>
+      <main>
+        <Dictionary />
+      </main>
       </header>
+      <footer>Coded by Kelly O'Keefe open-sourced on GitHub and hosted on Netlify</footer>
+      </div>
     </div>
   );
 }
